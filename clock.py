0b6600dbdb2d66df78f45a09b75a423ff9da7ce6
GHOSteD141 <167106526+GHOSteD141@users.noreply.github.com>
2024-05-17 20:38:13 +0530

    Create clock.py
    it is a clock in python

---
+++ clock.py
@@ -0,0 +1,22 @@
+# tkinter_clock.py
+
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+def update_clock(label):
+    string = strftime('%H:%M:%S:%p')
+    label.config(text=string)
+
+def schedule_update(label):
+    update_clock(label)
+    label.after(1000, lambda: schedule_update(label))
+
+root = Tk()
+root.title("Clock")
+
+clock_label = Label(root, font=("ds-digital", 80), background="red", foreground="green")
+clock_label.pack(anchor='center')
+
+schedule_update(clock_label)
+root.mainloop()
